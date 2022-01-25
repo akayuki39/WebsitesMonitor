7cacd0708346b1c03257b0389d8dee9c2499fdda
akayuki39 <593740901@qq.com>
2022-01-25 11:01:05 +0800

    change html mail style

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def sitesToMail(subject, sites, sendAsHtml=True, encoding=None):
 
     if sendAsHtml:
         for site in sites:
-            content = '<p><a href="' + site['uri'] + '">' + site['name'] + '</a></p>\n' + site['changes'] + '\n\n<hr>'
+            content = '<h2><a href="' + site['uri'] + '">' + site['name'] + '</a></h2>\n' + site['changes'] + '\n\n<hr>'
             mail_content += content
         mail_content = re.sub('\<hr>$', '', mail_content)
         mail = buildMailBody(subject, mail_content, None, sendAsHtml, encoding)
